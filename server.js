d0fc63eb1d8bd2c91d65db2c75db51725ea7fce1
Jeroen <Jeroenvinke17@gmail.com>
2016-03-15 13:47:04 +0100

    no message

--- server.js
+++ server.js
@@ -11,14 +11,32 @@ function handler (request, response ) {
  console.log("Response sent..");
 }
 
-//add this to server.js
-
 io.listen(server);
+
+var game = {
+};
 
 io.sockets.on("connection", function(socket) {
   console.log("user connected: " + socket.id);
 
   proxy(socket, 'PlayerMoved');
+
+  socket.on('JoinGame', function(playerInfo) {
+
+    if(game.player1 && game.player2) game = {};
+
+    if(!game.player1) {
+      game.player1 = playerInfo;
+    } else if(!game.player2) {
+      game.player2 = playerInfo;
+    }
+
+    socket.emit('PlayerJoined', game);
+
+    if(game.player1 && game.player2) {
+      socket.broadcast.emit('GameStart', game);
+    }
+  });
 });
 
 function proxy(socket, event) {
