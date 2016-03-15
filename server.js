7119524c1ed496983a410122ee5b307cb16d91c3
Gazar Ajroemjan <apero@live.nl>
2016-03-15 15:54:54 +0100

    Gamestatus terug

--- server.js
+++ server.js
@@ -25,6 +25,22 @@ io.sockets.on("connection", function(socket) {
     socket.emit('GameStatus', game);
   });
 
+  socket.emit('GameStatus', game);
+
+  socket.on('Ready', function(data){
+    if(data == 'player1'){
+      game.player1.ready = 'ready';
+    } else if(data == 'player2'){
+      game.player2.ready = 'ready';
+    }
+
+    if(game.player1.ready=='ready'&&game.player2.ready=='ready') {
+      io.sockets.emit('GameStart', game);
+      console.log('GameStart');
+    }
+  })
+
+
   socket.on('JoinGame', function(playerInfo) {
 
     // nieuw spel starten als alle plekken bezet zijn
