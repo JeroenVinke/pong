bb1c654640594d2d41270f8bdd8f8973f3c8cd11
Edd92 <edwinvanhalem@gmail.com>
2016-03-30 16:20:49 +0200

    Added test cases & fixed small loading bug

--- js/splashscreen.js
+++ js/splashscreen.js
@@ -32,7 +32,7 @@ socket.on('PlayerJoined', function (data) {
   if(data.player1 && data.player2) {
     $("#playerTableBody").append("<tr class='loading'><td colspan='3'>loading game!..........</td></tr>");
     var newcontent ="<div class='sk-circle'><div class='sk-circle1 sk-child'></div><div class='sk-circle2 sk-child'></div><div class='sk-circle3 sk-child'></div><div class='sk-circle4 sk-child'></div><div class='sk-circle5 sk-child'></div> <div class='sk-circle6 sk-child'></div> <div class='sk-circle7 sk-child'></div> <div class='sk-circle8 sk-child'></div> <div class='sk-circle9 sk-child'></div> <div class='sk-circle10 sk-child'></div> <div class='sk-circle11 sk-child'></div> <div class='sk-circle12 sk-child'></div> </div>";
-    $('.left').empty().append(newcontent);
+    $('.join').empty().append(newcontent);
     setTimeout(GoToGame, 4000);
 
   }
